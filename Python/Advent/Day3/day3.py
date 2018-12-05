1d61c060f54189f3c63ef05c6ba540aa7a9c5b39
Foopert <foopert@gmail.com>
2018-12-04 20:00:32 -0500

    ADD: Solved Day 3 Part 1 (also added print functionality)

---
+++ Python/Advent/Day3/day3.py
@@ -0,0 +1,84 @@
+# Day 3 of Advent Calendar code problem - Part 1
+
+import sys
+file = open("in.txt","r")
+str_arr = file.readlines()
+claim_arr = []
+
+# Creates 2D patch array initialized with dots
+patch_arr = [['.' for x in range(1000)] for y in range(1000)]
+# Grabs file lines and puts them into list, removes newline characters
+for x in range(len(str_arr)):
+    y = str_arr[x].replace('\n', '')
+    claim_arr.append(y)
+    
+
+# Whole piece of frabic is atleast 1000x1000 inches.
+# Each elf makes a claim of which area of said fabric is best.
+# Claims have an ID, specify area of rectangle. 
+# ClaimID: @ 3 inches (x coor), 2 inches (y coor). 5 wide and 4 tall.
+# Problem: Some claims overlap.
+
+# claimIDArr = [] # I don't know if claim ID's will be necessary
+
+# Pretty print patch array
+def printPatchArr(my_arr):
+    santas_strip = ''
+    for i in range(len(my_arr)):
+        patch_row = str(my_arr[i])
+        # I dont fully understand what this line is doing, but ey it removes the gross chars
+        patch_row = patch_row.translate({ord(c): None for c in '[\',] '})
+        santas_strip = santas_strip + patch_row + '\n' 
+    print(santas_strip)
+
+# Count collisions (!'s)
+def countCollisions(my_arr):
+    num_collisions = 0
+    for i in range(len(my_arr)):
+        for j in range(len(my_arr)):
+            if my_arr[i][j] == '!':
+                num_collisions = num_collisions + 1
+    print("\n" + str(num_collisions) + " COLLISIONS FOUND\n")
+
+'''TESTING'''
+#test_patch_arr = [['.' for x in range(10)] for y in range(10)]
+#test_claim_arr = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2', '#4 @ 5,5: 2x2', '#5 @ 5,5: 2x2']
+# Take in claim data and spit out useful data (coords of patch and size of patch)
+for i in range(len(claim_arr)):
+    data = claim_arr[i].split()
+
+    # Remove first claim ID and @ symbols from array
+    del data[0]
+    del data[0]
+    #print(data)
+
+    # Grab coord and dimension data
+    x_coord_data = int((data[0].split(','))[0])
+    y_coord_data = int((data[0].split(','))[1].rstrip(':'))
+    x_length = int(data[1].split('x')[0])
+    y_height = int(data[1].split('x')[1])
+
+    # This plots where the elves want to cut.
+    # . indicates empty, X indicates taken, ! indicates collision
+    for x in range(x_coord_data, x_coord_data + x_length):
+        for y in range(y_coord_data, y_coord_data + y_height):
+            if patch_arr[x][y] == '.':
+                patch_arr[x][y] = 'X'
+            elif patch_arr[x][y] == 'X':
+                patch_arr[x][y] = '!'
+    
+printPatchArr(patch_arr)
+countCollisions(patch_arr)
+
+
+    #print(data)
+
+# Create empty 2D Array
+#myArr = [[0 for x in range(1000)] for y in range(1000)]
+#print(myArr)
+
+
+
+    
+
+
