18dc71281eef5b59b0839080c8f2dcdcc87e9af9
ConnorMaloney <foopert@gmail.com>
2019-12-08 22:41:48 -0500

    Solved day 1!

--- 2019/Ruby/Day1/day1.rb
+++ 2019/Ruby/Day1/day1.rb
@@ -1,7 +1,16 @@
 arr = []
 File.open("input.txt").each do |line|
-    num = line.to_i 
-    num = (num / 3) - 2 
+    num = line.to_i
+    num = (num / 3) - 2
+    def recur_fuel(num)
+        if num <= 7
+            num
+        else
+            num = num + recur_fuel(num/3 - 2)
+        end
+    end
+    num = recur_fuel(num)
+            
     arr.push(num)
 end
 sum = arr.inject(0, :+)
